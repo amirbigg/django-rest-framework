b5fc28917d3baa3433322709ec100127249ab7e5
amirbig <amirbig44@gmail.com>
2022-04-01 12:40:25 +0430

    add ep1

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 app_name = 'home'
 urlpatterns = [
-	path('', views.Home.as_view(), name='home'),
+	path('', views.Home.as_view(), name='home'), # endpoint
 ]
--- home/views.py
+++ home/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
-from django.views import View
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 
-class Home(View):
+class Home(APIView):
 	def get(self, request):
-		return render(request, 'home/home.html')
+		return Response({'name':'jack'})
