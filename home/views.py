0a8cc396bbdf14a74c900ef8fd46c35ac6c79399
amirbig <amirbig44@gmail.com>
2022-04-01 13:09:08 +0430

    add ep2

--- home/views.py
+++ home/views.py
@@ -5,3 +5,7 @@ from rest_framework.response import Response
 class Home(APIView):
 	def get(self, request):
 		return Response({'name':'jack'})
+
+	def post(self, request):
+		name = request.data['name']
+		return Response({'name':name})
