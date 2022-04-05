0b5249e3f23c4556b3647ec5303703389e188cd9
amirbig <amirbig44@gmail.com>
2022-04-05 11:29:42 +0430

    add ep5

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,7 +1,22 @@
 from rest_framework import serializers
 
 
+def clean_email(value):
+	if 'admin' in value:
+		raise serializers.ValidationError('admin cant be in email')
+
 class UserRegisterSerializer(serializers.Serializer):
 	username = serializers.CharField(required=True)
-	email = serializers.EmailField(required=True)
+	email = serializers.EmailField(required=True, validators=[clean_email])
 	password = serializers.CharField(required=True, write_only=True)
+	password2 = serializers.CharField(required=True, write_only=True)
+
+	def validate_username(self, value):
+		if value == 'admin':
+			raise serializers.ValidationError('username cant be `admin`')
+		return value
+
+	def validate(self, data):
+		if data['password'] != data['password2']:
+			raise serializers.ValidationError('passwords must match')
+		return data
