06ed6bdf1997eca849a55c6f1b4e6aa9e683da29
amirbig <amirbig44@gmail.com>
2022-04-06 14:49:41 +0430

    add ep6

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,15 +1,21 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 
 
 def clean_email(value):
 	if 'admin' in value:
 		raise serializers.ValidationError('admin cant be in email')
 
-class UserRegisterSerializer(serializers.Serializer):
-	username = serializers.CharField(required=True)
-	email = serializers.EmailField(required=True, validators=[clean_email])
-	password = serializers.CharField(required=True, write_only=True)
-	password2 = serializers.CharField(required=True, write_only=True)
+class UserRegisterSerializer(serializers.ModelSerializer):
+	password2 = serializers.CharField(write_only=True, required=True)
+
+	class Meta:
+		model = User
+		fields = ('username', 'email', 'password', 'password2')
+		extra_kwargs = {
+			'password': {'write_only':True},
+			'email': {'validators': (clean_email,)}
+		}
 
 	def validate_username(self, value):
 		if value == 'admin':
