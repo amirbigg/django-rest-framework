43fbd77c5ef6f0b4b91b951fe4a04ca450a54ff9
amirbig <amirbig44@gmail.com>
2022-04-06 17:43:05 +0430

    add ep8

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import UserRegisterSerializer
+from rest_framework import status
 
 
 class UserRegister(APIView):
@@ -8,5 +9,5 @@ class UserRegister(APIView):
 		ser_data = UserRegisterSerializer(data=request.POST)
 		if ser_data.is_valid():
 			ser_data.create(ser_data.validated_data)
-			return Response(ser_data.data)
-		return Response(ser_data.errors)
+			return Response(ser_data.data, status=status.HTTP_201_CREATED)
+		return Response(ser_data.errors, status=status.HTTP_400_BAD_REQUEST)
