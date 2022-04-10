dd7630da4c8b5730f0ebecde4ad2723e28df0ab7
amirbig <amirbig44@gmail.com>
2022-04-10 12:58:54 +0430

    add ep9

--- A/settings.py
+++ A/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'home.apps.HomeConfig',
     'accounts.apps.AccountsConfig',
     'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# REST_FRAMEWORK
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from rest_framework.authtoken import views as auth_token
 
 
 app_name = 'accounts'
 urlpatterns = [
 	path('register/', views.UserRegister.as_view()),
+	path('api-token-auth/', auth_token.obtain_auth_token),
 ]
