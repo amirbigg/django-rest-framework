dbaf9cf0888dd607496a5ff05a187d2d5b4844c7
amirbig <amirbig44@gmail.com>
2022-04-14 13:25:08 +0430

    add ep12

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,5 @@ app_name = 'home'
 urlpatterns = [
 	path('', views.Home.as_view(), name='home'), # endpoint
 	path('questions/', views.QuestionView.as_view()),
+	path('questions/<int:pk>/', views.QuestionView.as_view()),
 ]
--- home/views.py
+++ home/views.py
@@ -1,3 +1,5 @@
+import dataclasses
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Person, Question, Answer
@@ -22,10 +24,21 @@ class QuestionView(APIView):
 		return Response(srz_data, status=status.HTTP_200_OK)
 
 	def post(self, request):
-		pass
+		srz_data = QuestionSerializer(data=request.data)
+		if srz_data.is_valid():
+			srz_data.save()
+			return Response(srz_data.data, status=status.HTTP_201_CREATED)
+		return Response(srz_data.errors, status=status.HTTP_400_BAD_REQUEST)
 
 	def put(self, request, pk):
-		pass
+		question = Question.objects.get(pk=pk)
+		srz_data = QuestionSerializer(instance=question, data=request.data, partial=True)
+		if srz_data.is_valid():
+			srz_data.save()
+			return Response(srz_data.data, status=status.HTTP_200_OK)
+		return Response(srz_data.errors, status=status.HTTP_400_BAD_REQUEST)
 
 	def delete(self, request, pk):
-		pass
+		question = Question.objects.get(pk=pk)
+		question.delete()
+		return Response({'message': 'question deleted'}, status=status.HTTP_200_OK)
