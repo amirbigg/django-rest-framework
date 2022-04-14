31214be8fc3ef26978757146f0e7c771f5b70aed
amirbig <amirbig44@gmail.com>
2022-04-14 10:39:08 +0430

    add ep10

--- home/views.py
+++ home/views.py
@@ -2,11 +2,13 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Person
 from .serializers import PersonSerializer
+from rest_framework.permissions import AllowAny
 
 
 class Home(APIView):
-	def get(self, request):
-		persons = Person.objects.get(name='amir')
-		ser_data = PersonSerializer(instance=persons)
-		return Response(data=ser_data.data)
+	permission_classes = [AllowAny,]
 
+	def get(self, request):
+		persons = Person.objects.all()
+		ser_data = PersonSerializer(instance=persons, many=True)
+		return Response(data=ser_data.data)
